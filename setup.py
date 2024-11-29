9bf9e023be6cc3b48146ef88813115301d46befb
Rahulact <rahul.jagtap@activense.com>
2024-11-30 00:35:32 +0530

    logging

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages,setup
 from typing import List 
 
+HYPEN_E_DOT = '-e .'
+
 def get_requirments(file_path:str)->List[str]:
     '''
     this function will return the list of requirments 
@@ -9,6 +11,12 @@ def get_requirments(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirments=file_obj.readlines()
         requirments=[ req.replace("/n","") for req in requirments]  ## because it txt file has the /n as new line
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+
+    return requirments        
+
+
 setup(
     name = 'mlproject',
     version='0.0.1',
